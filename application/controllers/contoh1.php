72b4da158be574156eceafb2ef74135d1f9e43b2
anselmus2002 <anselmus20020@gmail.com>
2021-09-20 14:14:09 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Anselmus Asioliong
+              Saya tinggal di Kabupaten Ketapang tepatnya di Desa Mahawa
+              Olahraga yang saya sukai adalah Batminton";
+    }
+}

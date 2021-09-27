9f0ea5f55b97188af7360120b901766b995ad98e
anselmus2002 <anselmus20020@gmail.com>
2021-09-27 11:43:12 +0700

    Menambahkan file web.php

---
+++ application/controllers/Web.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit ('no direct script access allowed');
+
+Class Web extends CI_Controller{
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        $data['judul'] = "Halaman Depan";
+        $this->loat->view('v_header',$data);
+        $this->loat->view('v_index',$data);
+        $this->loat->view('v_footer',$data);
+    }
+}
